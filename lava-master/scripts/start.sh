9caf5051d3a74a188228150f71062735a2e1f93f
Wei Gao <wei.gao@windriver.com>
2018-07-25 14:37:27 +0800

    Add lava-master/scripts/start.sh, to run as /etc/rc.local
    Signed-off-by: Wei Gao <wei.gao@windriver.com>

--- lava-master/scripts/start.sh
+++ lava-master/scripts/start.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-postgres-ready () {
-  echo "Waiting for lavaserver database to be active"
-  while (( $(ps -ef | grep -v grep | grep postgres | grep lavaserver | wc -l) == 0 ))
-  do
-    echo -n "."
-    sleep 1
-  done
-  echo 
-  echo "[ ok ] LAVA server ready"
+start_service () {
+	echo "Starting $1"
+	if (( $(ps -ef | grep -v grep | grep -v add_device | grep -v dispatcher-config | grep "$1" | wc -l) > 0 ))
+	then
+		echo "$1 appears to be running"
+		else
+		systemctl start "$1"
+	fi
 }
 
-start () {
-  echo "Starting $1"
-  if (( $(ps -ef | grep -v grep | grep -v add_device | grep -v dispatcher-config | grep "$1" | wc -l) > 0 ))
-  then
-    echo "$1 appears to be running"
-  else
-    systemctl start "$1"
-  fi
+
+main () {
+	echo "Waiting for lavaserver database to be active"
+	while (( $(ps -ef | grep -v grep | grep postgres | grep lavaserver | wc -l) == 0 ))
+	do
+		start_service postgresql
+		start_service apache2
+		start_service lava-master
+		start_service lava-coordinator
+		start_service lava-slave
+		start_service lava-server-gunicorn
+		start_service tftpd-hpa
+		echo -n "."
+		sleep 1
+	done
+	echo 
+	echo "[ ok ] LAVA server ready"
 }
 
 #remove lava-pid files incase the image is stored without first stopping the services
 rm -f /var/run/lava-*.pid 2> /dev/null
 
-start postgresql
-start apache2
-start lava-master
-start lava-coordinator
-start lava-slave
-start lava-server-gunicorn
-start tftpd-hpa
-
-postgres-ready
+main
 #added after the website not running a few times on boot
 systemctl reload apache2 
