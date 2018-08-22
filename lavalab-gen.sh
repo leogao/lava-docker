ae433cc901e7a65eb2dee5a23bd4f50322b4672f
Wei Gao <wei.gao@windriver.com>
2018-08-22 15:05:38 +0800

    Update lavalab-gen.sh, drop some unused steps.
    Signed-off-by: Wei Gao <wei.gao@windriver.com>

--- lavalab-gen.sh
+++ lavalab-gen.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 
-rm -rv lava-master/devices/
-rm -rv lava-master/slaves/
-rm -rv lava-slave/conmux/
-rm -rv lava-master/tokens/
-rm -rv lava-master/users/
-rm lavalab*rules
-
-if [ "$1" = "mrproper" ];then
-	exit 0
-fi
-
-./lavalab-gen.py || exit 1
-
 #check for root
 BEROOT=""
 if [ $(id -u) -ne 0 ];then
 	BEROOT="sudo "
 fi
-$BEROOT rm /etc/udev/rules.d/*lavalab*rules
-$BEROOT cp *lavalab*rules /etc/udev/rules.d/
 $BEROOT udevadm control --reload-rules || exit $?
 $BEROOT udevadm trigger || exit $?
 
