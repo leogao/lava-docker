30f62e7c49de3df4ee8e8f524e39403af680fb38
Wei Gao <wei.gao@windriver.com>
2018-08-22 15:04:14 +0800

    Add lava-slave scripts, fstab and start.sh
    Signed-off-by: Wei Gao <wei.gao@windriver.com>

--- lava-slave/scripts/start.sh
+++ lava-slave/scripts/start.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
-# Set LAVA Master IP
-if [[ -n "$LAVA_MASTER" ]]; then
-	sed -i -e "s/{LAVA_MASTER}/$LAVA_MASTER/g" /etc/lava-dispatcher/lava-slave
-fi
 
-systemctl start tftpd-hpa || exit 4
+main() {
+	echo "Waiting for lava-slave to be active"
+	loop=10
+	while (( $(ps -ef | grep -v grep | grep lava-slave | wc -l) == 0 ))
+	do
+		# Set LAVA Master IP
+		if [[ -n "$LAVA_MASTER" ]]; then
+			sed -i -e "s/{LAVA_MASTER}/$LAVA_MASTER/g" /etc/lava-dispatcher/lava-slave
+		fi
+		systemctl restart tftpd-hpa
+		# FIXME lava-slave does not run if old pid is present
+		rm -f /var/run/lava-slave.pid
+		systemctl restart lava-slave
+		if [[ $loop -le 0 ]]; then
+			echo "Failed to start lava-slave"
+			break
+		else
+			loop=$((loop-1))
+		fi
+		sleep 3
+	done
+	cd /opt/lava-extra/additions && bash setup_lava.sh
+	systemctl restart lava-slave || exit 5
+}
 
-# FIXME lava-slave does not run if old pid is present
-rm -f /var/run/lava-slave.pid
-systemctl start lava-slave || exit 5
-
-#if [ -e /etc/lavapdu ];then
-#	/etc/init.d/postgresql start || exit 6
-#
-#	echo "== Start lavapdu listen =="
-#	/etc/init.d/lavapdu-listen start || exit 7
-#	# lava listen create the database, let it some time
-#	sleep 5
-#	echo "== Start lavapdu runner =="
-#	/etc/init.d/lavapdu-runner start || exit 8
-#fi
-
-#touch /var/run/conmux-registry
-#/usr/sbin/conmux-registry 63000 /var/run/conmux-registry&
-#sleep 2
-#for item in $(ls /etc/conmux/*cf)
-#do
-#	echo "Add $item"
-#	/usr/sbin/conmux $item &
-#done
-
+main
 # start an http file server for boot/transfer_overlay support
 (cd /var/lib/lava/dispatcher; python -m SimpleHTTPServer 80)
