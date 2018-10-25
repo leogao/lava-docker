c5ef1800b9a81795bd3f3587aacef954b7c8d870
Wei Gao <wei.gao@windriver.com>
2018-10-25 14:57:31 +0800

    Update lava-slave/scripts/start.sh
    Modify LAVA_MASTER in lava-slave config file by the environment parameter
    which's transfered from docker-compose.yml while it build-up
    
    Signed-off-by: Wei Gao <wei.gao@windriver.com>

--- lava-slave/scripts/start.sh
+++ lava-slave/scripts/start.sh
@@ -1,30 +1,3 @@
 #!/bin/bash
-
-main() {
-	echo "Waiting for lava-slave to be active"
-	loop=10
-	while (( $(ps -ef | grep -v grep | grep lava-slave | wc -l) == 0 ))
-	do
-		# Set LAVA Master IP
-		if [[ -n "$LAVA_MASTER" ]]; then
-			sed -i -e "s/{LAVA_MASTER}/$LAVA_MASTER/g" /etc/lava-dispatcher/lava-slave
-		fi
-		systemctl restart tftpd-hpa
-		# FIXME lava-slave does not run if old pid is present
-		rm -f /var/run/lava-slave.pid
-		systemctl restart lava-slave
-		if [[ $loop -le 0 ]]; then
-			echo "Failed to start lava-slave"
-			break
-		else
-			loop=$((loop-1))
-		fi
-		sleep 3
-	done
-	cd /opt/lava-extra/additions && bash setup_lava.sh
-	systemctl restart lava-slave || exit 5
-}
-
-main
-# start an http file server for boot/transfer_overlay support
-(cd /var/lib/lava/dispatcher; python -m SimpleHTTPServer 80 &)
+sed -i -e "s/{LAVA_MASTER}/$LAVA_MASTER/g" /etc/lava-dispatcher/lava-slave
+exec /sbin/init 
