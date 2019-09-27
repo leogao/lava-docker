b780c59040ae7d687b407e4166b64e3f09ed0bbc
Wei Gao <wei.gao@windriver.com>
2019-09-27 17:06:16 +0800

    Setting LAVA_MASTER as the worker's coordinator.
    Signed-off-by: Wei Gao <wei.gao@windriver.com>

--- lava-slave/scripts/start.sh
+++ lava-slave/scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sed -i -e "s/{LAVA_MASTER}/$LAVA_MASTER/g" /etc/lava-dispatcher/lava-slave
+sed -i -e "s/localhost/$LAVA_MASTER/g" /etc/lava-coordinator/lava-coordinator.conf
 exec /sbin/init 
